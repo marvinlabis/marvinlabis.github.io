169b8fe7858418eaf742cdcbf6c2d6f7d2e997a6
Paul Labis <paul@hulk>
2015-10-27 14:13:30 +0800

    material design with angular opa concept

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/**
+ * AngularJS Tutorial 1
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ */
+
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('profileWebApp', [
+  'ngRoute'
+]);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/work", {templateUrl: "partials/work.html", controller: "PageCtrl"})
+    .when("/speak", {templateUrl: "partials/speak.html", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+});

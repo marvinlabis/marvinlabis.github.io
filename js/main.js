b5ee31c09f964e406f444396316cd7cca03d32f6
Paul Labis <paul@hulk>
2015-10-29 03:39:43 +0800

    updating pages, active menu and adding work experience

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/work", {templateUrl: "partials/work.html", controller: "PageCtrl"})
+    .when("/work", {templateUrl: "partials/work.html", controller: "PageWorkCtrl"})
     .when("/speak", {templateUrl: "partials/speak.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
     // else 404
@@ -31,7 +31,26 @@ app.config(['$routeProvider', function ($routeProvider) {
 /**
  * Controls all other Pages
  */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
+app.controller('PageCtrl', function ( /*$scope, $location, $http */) {
+    console.log("Page Controller reporting for duty.");
+});
 
+app.controller('PageWorkCtrl', function ( /*$scope, $location, $http */) {
+    $('.collapsible').collapsible({
+            accordion : false 
+          });
+});
+
+app.controller('MenuCtrl', function ($scope, $location) {
+    $scope.getClass = function(path) {
+        var cur_path = $location.path().substr(0, path.length);
+        if (cur_path == path) {
+            if($location.path().substr(0).length > 1 && path.length == 1 )
+                return "";
+            else
+                return "active";
+        } else {
+            return "";
+        }
+    }
 });
--- js/materializeInit.js
+++ js/materializeInit.js
@@ -2,6 +2,9 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-
+    
+    $('.collapsible').collapsible({
+      accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
